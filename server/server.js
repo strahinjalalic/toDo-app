00a55e49238c1fe8baf6273ff40e9baeaf7b665f
someone <someone@something.com>
2019-03-14 15:11:55 +0100

    added new patch route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 const {ObjectID} = require("mongodb");
+const _ = require("lodash");
+
 
 app.use(bodyParser.json());
 
@@ -52,6 +54,31 @@ app.get("/todos/:id", (req, res) => {
     }
 });
 
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]); //pick() je metod koji nam omogucava da odredimo stvari koje user moze da update-uje => van ova dva property-ja ne moze nista => prvi argument je req.body, jer se izvlace info iz njega, a drugi argument je niz sa stavkama koje zelimo da omogucimo useru da update-uje
+
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+       body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {//new:true => slicno returnOriginal property-ju
+     if(!todo) {
+      return res.status(404).send();
+     }
+
+     res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  }); 
+});
 
 app.delete("/todos/:id", (req, res) => {
   var id = req.params.id;
