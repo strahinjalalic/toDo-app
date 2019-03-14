f3a5b7fb185e9a9be9efa12d6baf19c823dd1cf2
someone <someone@something.com>
2019-03-14 14:36:08 +0100

    tested removing a todo

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -87,3 +87,27 @@ describe("GET /todos/:id", () => {
 		 .end(done);
 	})
 });
+
+
+describe("DELETE /todos/:id", () => {
+	it("should remove todo", (done) => {
+      var hexId = todos[1]._id.toHexString();
+
+      request(app)
+       .delete(`/todos/${hexId}`)
+       .expect(200)
+       .expect((res) => {
+       	 expect(res.body.todo._id).toBe(hexId)
+       })
+       .end((err, res) => {
+       	 if(err) {
+       	 	return done(err);
+       	 }
+
+       	 Todo.findById(hexId).then((todo) => {
+       	 	expect(todo).toNotExist();
+       	 	done();
+       	 });
+       });
+	});
+});
