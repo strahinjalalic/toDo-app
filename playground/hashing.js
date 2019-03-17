724f183363d36b23d03ee6468f9cf39e3aec9a04
someone <someone@something.com>
2019-03-17 15:29:08 +0100

    new hash playground

---
+++ playground/hashing.js
@@ -0,0 +1,43 @@
+var {SHA256} = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+var data = {
+	id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log(decoded);
+
+
+// var message = "Poruka koja ce biti kriptovana!";
+// var hash = SHA256(message).toString(); //poziva se toString() metod zato sto je rezultat u vidu objekta
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+
+// var data = {
+// 	id: 4
+// };
+
+// var token = {
+// 	data,
+// 	hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(data)).toString();
+
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// if(resultHash === token.hash) { //proveravamo da li je na kraju hashirana vrednost jednaka pocetnoj iz objekta(korismo saulting za verifikovanje=> 'somesecret')
+// 	console.log("Data has not been changed");
+// } else {
+// 	console.log("Data has been changed!");
+// }
+//ceo ovaj postupak se zove JWT(JSONWebToken) i mnogo ga je lakse koristiti preko biblioteke jsonwebtoken
+
